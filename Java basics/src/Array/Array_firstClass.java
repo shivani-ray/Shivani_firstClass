d1a6f73d645dc3396a516eb32c8919cb6e04c6e5
shivani <shivaniray0105@gmail.com>
2026-06-18 22:03:43 +0530

    Initial commit

---
+++ Java basics/src/Array/Array_firstClass.java
@@ -0,0 +1,15 @@
+package Array;
+
+public class Array_firstClass {
+    static void main(String[] args) {
+        //int [][] arr_1=new int[5][3];
+        int[][] arr = {{50, 12, 10},{15,20,23},{43,45,66}};
+
+        // multiarray//
+        System.out.println(arr[0][0]);
+        System.out.println(arr[1][1]);
+        System.out.println(arr[0][2]);
+
+
+    }
+}
