177a8ad7973ffbf93ba3f4f0eb63e9d8e3a1f3de
shivani <shivaniray0105@gmail.com>
2026-06-21 21:56:00 +0530

    Initial commit

---
+++ Java basics/src/Array/search_element.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class search_element {
+
+    static void main(String[] args) {
+        int [] arr= {1,2,4,5,6};
+        int x=4;
+        int ans =1;
+        for( int i=0 ; i<arr.length;i++)
+        {
+            if(arr[i]==x){
+                ans=i;
+                System.out.println("found" + x + "at index"+ans);
+
+
+                break;
+
+            }
+
+
+        }
+    }
+}
